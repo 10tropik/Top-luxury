8783a8ff8bcf0f931aed0c95c570208d95343944
10tropik <crypto.shopp@hotmail.com>
2026-01-13 13:50:38 +0100

    Update App.js

--- App.js
+++ App.js
@@ -54,3 +54,15 @@ async function loadProducts() {
 }
 
 loadProducts();
+
+
+
+// Animation supplémentaire si besoin
+document.querySelectorAll('.case').forEach(item => {
+    item.addEventListener('click', () => {
+        item.style.boxShadow = '0 0 30px #ffcc00, 0 0 60px #ffcc00';
+        setTimeout(() => {
+            item.style.boxShadow = '';
+        }, 300);
+    });
+});
